9eb822f8937b289efaa65d2aa96866e1413eecb0
biopravda <30368360+biopravda@users.noreply.github.com>
2018-03-14 12:46:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 class SmartCalculator {
   constructor(initialValue) {
+    this.result=String(initialValue);
     // your implementation
   }
 
   add(number) {
+    this.result=this.result+ '+' +number;
+    return this;
     // your implementation
   }
   
   subtract(number) {
+    this.result=this.result+ '-' +number;
+    return this;
     // your implementation
   }
 
   multiply(number) {
-    // your implementation
+    this.result=this.result+ '*' +number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.result=this.result+ '/' +number;
+    return this;   
   }
 
   pow(number) {
     // your implementation
+    this.result=this.result+ '**' +number;
+    return this;
   }
+
+  toString() {
+  return eval(this.result);
+}
 }
 
 module.exports = SmartCalculator;
